f9560a87d65161725b6be6a1002d5e2f822b3ef3
DouglasBruce <douglasbruce96@gmail.com>
2023-09-26 23:41:30 +0100

    Created register page.

--- app/src/main/java/com/douglasbruce/tasky/core/designsystem/icon/TaskyIcons.kt
+++ app/src/main/java/com/douglasbruce/tasky/core/designsystem/icon/TaskyIcons.kt
@@ -2,11 +2,13 @@ package com.douglasbruce.tasky.core.designsystem.icon
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.NavigateBefore
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 
 object TaskyIcons {
+    val Check = Icons.Filled.Check
+    val NavigateBefore = Icons.Filled.NavigateBefore
     val Visibility = Icons.Filled.Visibility
     val VisibilityOff = Icons.Filled.VisibilityOff
-    val Check = Icons.Filled.Check
 }
