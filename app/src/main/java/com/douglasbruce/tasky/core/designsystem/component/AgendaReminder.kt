0a8a35683c8eb4d2f44a5aa4bc197deb3361891c
Douglas Bruce <douglasbruce96@gmail.com>
2023-10-16 23:48:33 +0100

    Merge pull request (#20) from 1.0.0-animate-edit-chevron

--- app/src/main/java/com/douglasbruce/tasky/core/designsystem/component/AgendaReminder.kt
+++ app/src/main/java/com/douglasbruce/tasky/core/designsystem/component/AgendaReminder.kt
@@ -1,5 +1,8 @@
 package com.douglasbruce.tasky.core.designsystem.component
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
@@ -51,16 +54,17 @@ fun AgendaReminder(
                 )
             )
         },
-        trailingContent = when (isReadOnly) {
-            true -> null
-            else -> {
-                {
-                    Icon(
-                        imageVector = TaskyIcons.ChevronRight,
-                        contentDescription = null,
-                        tint = Black,
-                    )
-                }
+        trailingContent = {
+            AnimatedVisibility(
+                visible = !isReadOnly,
+                enter = fadeIn(),
+                exit = fadeOut(),
+            ) {
+                Icon(
+                    imageVector = TaskyIcons.ChevronRight,
+                    contentDescription = null,
+                    tint = Black,
+                )
             }
         },
         modifier = modifier.clickable(enabled = !isReadOnly) { onClick() }
